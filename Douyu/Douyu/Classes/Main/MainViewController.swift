40155dc99a9205bf573145aee6e9fb63fcc1de76
xiangliyou <xiangliyou@aliyun.com>
2017-11-11 21:17:07 +0800

    项目框架搭建

---
+++ Douyu/Douyu/Classes/Main/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Douyu
+//
+//  Created by xiangliyou on 2017/11/11.
+//  Copyright © 2017年 xiangliyou. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
